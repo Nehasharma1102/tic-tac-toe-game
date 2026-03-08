453bef82a9bbf5f750e8e8b05746a8764378c5cc
Nehasharma1102 <s24_sharma_neha@mgmcen.ac.in>
2026-03-08 14:36:11 -0700

    Added Tic Tac Toe game files

---
+++ first.js
@@ -0,0 +1,98 @@
+let boxes =document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer= document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let count =0;
+
+let turnO=true;//playerX,playerY
+
+const winPatterns =[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+
+];
+
+const checkDraw = () => {
+    if(count === 9){
+        msg.innerText = "It's a Draw! 🤝";
+        msgContainer.classList.remove("hide");
+        disableBoxes();
+    }
+};
+
+const resetGame = () =>{
+    turnO=true;
+    enableboxes();
+    msgContainer.classList.add("hide");
+    count = 0;
+
+
+    
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        if(turnO){
+            box.innerText ="O";
+            turnO= false;
+        }else{
+            box.innerText ="X";
+            turnO= true;
+
+        }
+        box.disabled = true;
+        count++;
+
+        let winner = checkWinner();
+        if(!winner){
+            checkDraw();
+        }
+
+        checkWinner ();
+    });
+});
+
+const disableboxes = (winner) => {
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableboxes = (winner) => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerText="";
+    }
+}
+
+
+const showWinner = (winner) => {
+  msg.innerText=`Congratulations , winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableboxes();
+};
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText ;
+        let pos2Val = boxes[pattern[1]].innerText ;
+        let pos3Val = boxes[pattern[2]].innerText ;
+
+        if(pos1Val !="" && pos2Val !="" && pos3Val !=""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                showWinner(pos1Val);
+            }
+        }
+
+    }
+};
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
+
